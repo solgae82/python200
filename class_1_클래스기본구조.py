c9c9deecee29aec420e4f470a7a2ddcb77634a85
notesolgae82 <kehdgur@kakao.com>
2026-05-18 12:30:55 +0900

    파이썬 클래스 재정리 , 복습

--- class_1_클래스기본구조.py
+++ class_1_클래스기본구조.py
@@ -1,8 +1,18 @@
 
+# 파이썬은 
+# "명시적인 것이 암시적인 것보다 낫다
+# (Explicit is better than implicit)"
+# 는 철학을 가집니다.
+
+# 인스턴스 구조로 만들때는 
+# 반드시 첫벗째 인자에 인스턴스 변수를 전달 받을 매개변수를 넣는다
+# (코딩규약에 따라) 'self'명으로 넣는다.(다른 이름이어도 에러 아님)
+
+
 # 클래스 기본 구조
 class User: 
 
-    # 생성자 , self(자바의 this와 동일)
+    # 생성자 , self(자바의 this 개념)
     def __init__(self, name, age): 
         self.name = name
         self.age = age
@@ -13,3 +23,45 @@ class User:
     
 u = User("kim", 30)
 print(u.introduce()) # 이름은 'kim' / 나이는 30살 입니다
+
+"""
+내부적으로: 
+
+User.__init__(u,"kim", 30)
+User.introduce(u) 
+
+가 된다. 명시적으로..
+
+자바 클래스처럼 생각하면 오해가 생긴다.
+"""
+
+class Tclass:
+    def test():
+        print('test')
+
+t = Tclass()
+
+# t.test() # 에러,파이썬 실행기가 인수 1개를 무조건 넣는데, 선언엔 없으므로..
+# TypeError: Tclass.test() takes 0 positional arguments but 1 was given
+
+Tclass.test() # test
+
+"""
+파이썬에서는 클래스 안에 메소드 첫벗째 인자로 self가 기본.
+만약 넣지 않았다면, 클래스명.메소드로만 접근하겠다는 뜻.
+(파이썬에서는 이렇게 클래스 프로그래밍 하지 않는다)
+언제나 명시적으로! (self,..)
+"""
+
+"""
+인스턴스/클래스 인자 없이 test()로 선언하는 방법이 있다
+@staticmethod 데코레이터로 확장 (나중에 따로 정리 할 것이다)
+(클래스 기본이라기 보다는 독립 함수 느낌)
+"""
+class Tclass2:
+    @staticmethod
+    def test():
+        print('test')
+
+t = Tclass2()
+t.test() # test
--- class_4_클래스메서드_static메서드.py
+++ class_4_클래스메서드_static메서드.py
@@ -1,4 +1,4 @@
-# 클래스 메서드 / static 메서드
+# 클래스 메서드 / static 메서드 미리 보기
 class User:
 
     @classmethod
@@ -6,7 +6,7 @@ class User:
         return cls("default")
 
     @staticmethod
-    def hello(): # 아무것도 전달하지 않는다
+    def hello(): # self 인자 없다
         return "hello"
     
 """
@@ -49,16 +49,16 @@ class User:
         self.name = name
 
     @classmethod
-    def guest(cls):
+    def guest(cls): 
         return cls("guest")
     
 u = User.guest()
 print(u.name) # guest
 """
-Java 생성자 오버로딩 대신
+Java 생성자 오버로딩 같은 기능을 
 Python은 이름 있는 대체 생성자 패턴 메서드를 만든다
 
-Python은 오버로딩 없음.
+(Python은 오버로딩 없음)
 
 그래서:
 
@@ -71,19 +71,23 @@ Python은 오버로딩 없음.
 
 
 #-------- 3. staticmethod = 아무것도 안 받음
+# 
 class User:
     @staticmethod
-    def add(a, b):
-        return a + b
+    def t_msg():    # self 인자 없음
+        print('홍길동')
     
-sum = User.add(1,2)
-print(sum) # 3
+u = User()
+u.t_msg() # 홍길동
 """
-내부적으로 그냥 함수 , 클래스와 관련된 함수
+t_msg() 는
+내부적으로 인스턴스,클래스 인자 없이 독립적으로 실행되는 함수가 됨.
+(클래스와 관련된 실행된 함수)
+그래서 t_msg(self) 로 선언하지 않아도 실행되는 것임.
 """
 
 #-------- 요약 
 """
-classmethod = 생성 전략
-staticmethod = 관련 유틸 함수
+@classmethod = 생성 전략
+@staticmethod = 클래스 관련 독립 실행되는 유틸 함수
 """
