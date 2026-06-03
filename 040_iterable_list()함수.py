90a29cb028b88be2728a13f34f8c509947e037cc
solgae82 <kehdgur@gmail.com>
2026-06-03 22:30:50 +0900

    딕셔너리 정리 완료

--- 040_iterable_list()함수.py
+++ 040_iterable_list()함수.py
@@ -2,15 +2,18 @@
 # 모든 이터러블 객체(시퀀스,문자열,dictionary,set,binary sequence)는 list()로 리스트로 변환 할 수 있다
 # 제너레이터도 list() 로 리스트 변환 가능.
 
+# 시퀀스: 튜플
 tupleData = (1,2,3)
 listData = list(tupleData)
 print(type(listData), listData, sep=",") # <class 'list'>,[1, 2, 3]
 
+# 시퀀스: 문자열
 strData = 'I love python'
 listData = list(strData)
 print(type(listData), listData, sep=",") 
 #<class 'list'>,['I', ' ', 'l', 'o', 'v', 'e', ' ', 'p', 'y', 't', 'h', 'o', 'n']
 
+# 시퀀스:generator
 # 제너레이터 -> 리스트 변환
 def Myrange(n):
     current = 0
@@ -23,3 +26,23 @@ print(type(int_my_list)) # <class 'generator'>
 
 listData = list(int_my_list)
 print(type(listData), listData, sep=',') # <class 'list'>,[0, 1, 2, 3, 4]
+
+# set 자료형
+set_data = {'사과','배','딸기'}
+s_list = list(set_data)
+print(type(s_list), s_list) # <class 'list'> ['배', '사과', '딸기']
+
+# dictionary 자료형
+dic_data = {'사과':10, '배': 20, '바나나': 15}
+d_list = list(dic_data) # 기본 키값으로 리스트를 만든다
+print(type(d_list), d_list) # <class 'list'> ['사과', '배', '바나나']
+
+d_list = list(dic_data.items()) # 튜플로 리스트를 만든다
+print(type(d_list), d_list) # <class 'list'> [('사과', 10), ('배', 20), ('바나나', 15)]
+
+d_list = list(dic_data.keys()) # 명시적 키값으로 리스트를 만든다
+print(type(d_list), d_list) # <class 'list'> ['사과', '배', '바나나']
+
+d_list = list(dic_data.values()) # 명시적 값으로 리스트를 만든다
+print(type(d_list), d_list) # <class 'list'> [10, 20, 15]
+
--- 104-123_dictionary자료형.py
+++ 104-123_dictionary자료형.py
@@ -79,9 +79,12 @@ del p['서울']
 print(population) # {'서울': 100, '부산': 50}
 print(p) # {'부산': 50}
 
+
+# 원복 객체 참조로 items(), keys(), values() 조회
+
 # items() => dict_items([('key','value')])
 population = {'서울':100, '부산':50}
-p_items = population.items()
+p_items = population.items() # 딕셔너리 참조로 (키,값) 리스트 튜플 객체 반환
 print(type(p_items)) # <class 'dict_items'>
 print(p_items) # dict_items([('서울', 100), ('부산', 50)])
 
@@ -92,15 +95,14 @@ for k,v in p_items:
 부산 => 50
 '''
 
-# items() 객체와 원본 객체는 서로 연관 반영 된다.
+# 참조객체를 사용하므로 원본객체가 변경되면 즉시 반영된다.
 del population['서울'] # 원본 삭제
 print(population) # {'부산': 50}
 print(p_items) # dict_items([('부산', 50)]) , items()객체도 변한다
 
 # key() => dict_keys(['키값',..])
-
 population = {'서울':100, '부산':50}
-p_keys = population.keys()
+p_keys = population.keys() # 딕셔너리 참조로 키값 리스트 반환
 print(type(p_keys)) # <class 'dict_keys'>
 print(p_keys) # dict_keys(['서울', '부산'])
 
@@ -109,9 +111,109 @@ for v in p_keys:
 
 print()
 
-# keys() 객체와 원본 객체는 서로 연관 반영 된다.
+# 참조객체를 사용하므로 원본객체가 변경되면 즉시 반영된다.
 del population['부산']
 print(population) #{'서울': 100}
 print(p_keys) # dict_keys(['서울'])
 
+# values() => dict_values([값,..])
+population = {'서울':100, '부산':50}
+p_values = population.values() # 딕셔너리 참조로 값 리스트 반환
+print(type(p_values)) # <class 'dict_values'>
+print(p_values) # dict_values([100, 50])
+print(list(p_values)) # [100, 50]
+
+# 참조객체를 사용하므로 원본객체가 변경되면 즉시 반영된다.
+del population['서울']
+print(population) # {'부산': 50}
+print(p_values) # dict_values([50])
+
+# 내장함수 reversed 로 키 순서 역순 만들기
+population = {'서울':100, '대전':45, '부산':50}
+r_keys = reversed(population) # 키 값 기준으로 역순 반환
+print(type(r_keys)) # <class 'dict_reversekeyiterator'>
+print(list(r_keys)) # ['부산', '대전', '서울']
+
+# pop()
+population = {'서울':100, '대전':45, '부산':50}
+# p_value = population.pop('인천') #  키 값 없으면 에러, KeyError: '인천'
+# p_value = population.pop() # 인수 없으면 에러, TypeError: pop expected at least 1 argument, got 0
+p_value = population.pop('서울')
+print(p_value) # 100
+print(population) # {'대전': 45, '부산': 50}
+
+p_value = population.pop('인천', -1) # 해당 값이 없으면 두번째 인수값 반환
+print(p_value) # -1
+
+# popitem() => 마지막 요소 추출 튜플로 변환
+population = {'서울':100, '대전':45, '부산':50}
+p_item = population.popitem()
+print(type(p_item),p_item) # <class 'tuple'> ('부산', 50)
+print(population) # {'서울': 100, '대전': 45}
+
+# update(dict) => 인수 딕셔너리로 원본 갱신 및 추가
+population = {'서울':100, '대전':45}
+new_dict = {'대전':55, '부산':50}
+population.update(new_dict) # population |= new_dict 와 같다
+print(population) # {'서울': 100, '대전': 55, '부산': 50}
+
+population.update(인천=40) # 이 표현식도 가능하다, 주의사항 문자열'' 하면 안된다
+#population.update('인천'=40) # SyntaxError, 문자열 ''를 하면 에러 
+print(population) # {'서울': 100, '대전': 55, '부산': 50, '인천': 40}
+
+# 새 딕셔리너리로 조합,  새로운 dictionary 생성
+population = {'서울':100, '대전':45}
+u_dict = {'대전':55, '부산':50}
+new_dict = population | u_dict
+print(new_dict) # {'서울': 100, '대전': 55, '부산': 50}
+
+new_dict = population | {'인천': 45}
+print(new_dict) # {'서울': 100, '대전': 45, '인천': 45}
+
+population |= {'서울':1000, '울산':40} # 원본 변경, update(dict)와 같다
+print(population) # {'서울': 1000, '대전': 45, '울산': 40}
+
+# sorted 로 딕셔너리 오름차순 정렬 반환
+population = {'서울':100, '대전':45, '부산':50}
+new_dict = sorted(population) # 키 오름차순 정렬 리스트 반환
+print(type(new_dict),new_dict) # <class 'list'> ['대전', '부산', '서울']
+
+new_dict = sorted(population.items()) # 키 오름차순 정렬 튜플 반환
+print(type(new_dict)) # <class 'list'>
+print(new_dict) # [('대전', 45), ('부산', 50), ('서울', 100)]
+
+new_dict = sorted(population.keys()) # 키 오름차순 정렬 리스트 반환
+print(type(new_dict)) # <class 'list'>
+print(new_dict) # ['대전', '부산', '서울']
+
+new_dict = sorted(population.values()) # 값 오름차순 정렬 리스트 반환
+print(type(new_dict)) # <class 'list'>
+print(new_dict) # [45, 50, 100]
+
+# sorted 로 딕셔너리 내림차순 정렬 반환
+population = {'서울':100, '대전':45, '부산':50}
+new_dict = sorted(population, reverse=True) # 키 내림차순 정렬 리스트 반환
+print(type(new_dict),new_dict) # <class 'list'> ['서울', '부산', '대전']
+
+new_dict = sorted(population.items(), reverse=True) # 키 내림차순 정렬 튜플 반환
+print(type(new_dict)) # <class 'list'>
+print(new_dict) # [('서울', 100), ('부산', 50), ('대전', 45)]
+
+new_dict = sorted(population.keys(), reverse=True) # 키 내림차순 정렬 리스트 반환
+print(type(new_dict)) # <class 'list'>
+print(new_dict) # ['서울', '부산', '대전']
+
+new_dict = sorted(population.values(), reverse=True) # 값 내림차순 정렬 리스트 반환
+print(type(new_dict)) # <class 'list'>
+print(new_dict) # [100, 50, 45]
+
+# sorted, 람다식으로 값 기준 오름차순 정렬 튜플 반환
+population = {'서울':100, '대전':45, '부산':50}
+new_dict = sorted(population.items(), key=lambda x:x[1]) # [1] 값 기준으로 오름차순 정렬
+print(new_dict) # [('대전', 45), ('부산', 50), ('서울', 100)]
+
+# sorted, 람다식으로 값 기준 내림차순 정렬 튜플 반환
+population = {'서울':100, '대전':45, '부산':50}
+new_dict = sorted(population.items(), key=lambda x:x[1], reverse=True) # [1] 값 기준으로 내림차순정렬
+print(new_dict) # [('서울', 100), ('부산', 50), ('대전', 45)]
 
