2b09299c7d79ee8e5328c42439b43f8a651efe6e
solgae82 <kehdgur@gmail.com>
2026-09-14 01:04:32 +0900

    리스트 메소드 재정리

--- 056-066_리스트_메소드들.py
+++ 056-066_리스트_메소드들.py
@@ -1,12 +1,36 @@
+# 요소값 1개 가져오기
+n_list = ['태양', '지구', '목성']
+print(n_list[1]) # 지구
+
+# 요소값 추출 반환, [index], [index:], [[index:end_index]
+f_list = ['사과','배','수박','참외']
+print(f_list[1]) # 배
+print(f_list[1:]) # ['배', '수박', '참외']
+print(f_list[1:2]) # ['배']
+print(f_list[1:3]) # ['배', '수박']
+
 # 요소 추가
 n_list = ['태양', '지구', '목성']
 n_list.append('천왕성')
 print(n_list) # ['태양', '지구', '목성', '천왕성']
 
+# 요소 추가(끼워넣기), insert(인덱스, 값)
+n_list = ['태양', '지구', '목성']
+n_list.insert(1,'금성')
+print(n_list) # ['태양', '금성', '지구', '목성']
+
 # 요소 변경
 n_list = ['태양', '지구', '목성']
 n_list[1] = '금성'
 print(n_list) # ['태양', '금성', '목성']
+
+
+########### 요소 삭제 
+
+# 요소 삭제, del 키워드
+n_list = ['태양', '지구', '목성']
+del n_list[2]
+print(n_list) # ['태양', '지구']
 
 # 요소 삭제 , remove('요소")
 n_list = ['태양', '지구', '목성']
@@ -15,21 +39,29 @@ n_list = ['태양', '지구', '목성']
 n_list.remove('지구')
 print(n_list) # ['태양', '목성']
 
-# 요소 삭제
-n_list = ['태양', '지구', '목성']
-del n_list[1]
-print(n_list) # ['태양', '목성']
-
 # 요소 삭제, (중복 요소가 있을때는 가장 먼저 발견된 요소만 삭제)
 n_list = ['태양', '지구', '목성', '지구']
 n_list.remove('지구')
 print(n_list) #['태양', '목성', '지구']
+
+# 요소값 추출(마지막 자료 추출 후 자료구조 재조정)
+n_list = ['태양', '금성', '지구', '목성']
+last_pop = n_list.pop()
+print(last_pop, n_list) # 목성 ['태양', '금성', '지구']
+
+# 요소값 추출(인덱스로 추출 후 자료구조 재조정)
+n_list = ['태양', '금성', '지구']
+v_pop = n_list.pop(n_list.index('금성'))
+print(v_pop , n_list) # 금성 ['태양', '지구']
 
 # 모든 요소 삭제 , clear()
 n_list = ['태양', '지구', '목성']
 # del n_list[:] # == n_list.clear()
 n_list.clear()
 print(n_list) # []
+
+
+########### util 메소드 + 연산 
 
 # 요소 복사, copy()
 n_list = ['태양', '지구', '목성']
@@ -46,31 +78,11 @@ c_list = ['태양', '지구', '목성']
 e_list = c_list + ['천왕성','해왕성','명왕성']
 print(e_list) # ['태양', '지구', '목성', '천왕성', '해왕성', '명왕성']
 
-# 요소 추가(끼워넣기), insert(인덱스, 값)
-n_list = ['태양', '지구', '목성']
-n_list.insert(1,'금성')
-print(n_list) # ['태양', '금성', '지구', '목성']
-
 # 요소 인덱스 반환
 n_list = ['태양', '금성', '지구', '목성']
 print('지구index=>', n_list.index('지구')) # 지구index=> 2
 
-# 요소값 추출(마지막 자료 추출 후 자료구조 재조정)
-n_list = ['태양', '금성', '지구', '목성']
-last_pop = n_list.pop()
-print(last_pop, n_list) # 목성 ['태양', '금성', '지구']
-
-# 요소값 추출(인덱스로 추출 후 자료구조 재조정)
-n_list = ['태양', '금성', '지구']
-v_pop = n_list.pop(n_list.index('금성'))
-print(v_pop , n_list) # 금성 ['태양', '지구']
-
-# 요소값 추출 반환, [index], [index:], [[index:end_index]
-f_list = ['사과','배','수박','참외']
-print(f_list[1]) # 배
-print(f_list[1:]) # ['배', '수박', '참외']
-print(f_list[1:2]) # ['배']
-print(f_list[1:3]) # ['배', '수박']
+########### 재배치(정렬[오름,내림],역순변환)
 
 # 요소 역순 변환 (현재 자료구조 순서 역순 변경)
 n_list = ['태양', '지구', '목성']
