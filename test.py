b54cc688b071c1a1e5088795eb87c3f75b2b1544
solgae82 <kehdgur@gmail.com>
2026-09-14 02:41:33 +0900

    컬렉션요약 수정

--- test.py
+++ test.py
@@ -1,13 +1,8 @@
 # 출석부에 넣을 학생 이름 목록입니다.
-students = ["철수", "영희", "민수"]
+students = ["철수", "영희", "민수","영희"]
 
-# 1. 값(value)을 지정하지 않고 만들기
-# -> 기본적으로 'None'(아무것도 없음)이 들어갑니다.
-attendance_book1 = dict.fromkeys(students)
-print("출석부 1번:", attendance_book1)
-print("-" * 30)
+students.remove("영희")
+print(students)
 
-# 2. 값(value)을 '출석'으로 지정해서 한 번에 만들기
-# -> 모든 학생의 상태가 한 번에 '출석'으로 채워집니다.
-attendance_book2 = dict.fromkeys(students, "출석")
-print("출석부 2번:", attendance_book2)
+t = tuple(students)
+print(t)
