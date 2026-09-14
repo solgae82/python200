a3790d9408a072db04a2ec7c8696fc0900d1f0e2
solgae82 <kehdgur@gmail.com>
2026-09-14 09:40:48 +0900

    d['key'] 가져오기 추가

--- test.py
+++ test.py
@@ -1,8 +1,4 @@
-# 출석부에 넣을 학생 이름 목록입니다.
-students = ["철수", "영희", "민수","영희"]
 
-students.remove("영희")
-print(students)
+d = {'k1':'하루','k2':'이틀'}
 
-t = tuple(students)
-print(t)
+print(d['k1'],d.get('k1'),d)
