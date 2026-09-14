06677dd178d38b173339ac10151385a3a8006a21
solgae82 <kehdgur@gmail.com>
2026-09-14 10:18:16 +0900

    리스트 모두 가능(파이썬): pop(), pop(i)

--- test.py
+++ test.py
@@ -1,4 +1,23 @@
 
-d = {'k1':'하루','k2':'이틀'}
+s = {'하루','이틀','삼일'}
 
-print(d['k1'],d.get('k1'),d)
+s.add('사흘')
+print(s) # {'사흘', '삼일', '이틀', '하루'}
+
+v = s.remove('사흘')
+print(s , v) # {'하루', '이틀', '삼일'} None
+
+v = s.discard('삼일')
+print(s, v) # {'하루', '이틀'} None
+
+p = s.pop()
+print(p, s) # 이틀 {'하루'} 또는 하루 {'이틀'}
+# p = s.pop('이틀') # ! 에러, 인수 넣으면 안됨.
+
+s.clear()
+print(s) # set()
+
+ls = [1,2]
+
+v = ls.pop(0)
+print(v)
